ca544f90603fae33d7545b43ea7f7109e6c28a1c
lijq123 <lijq123@foxmail.com>
2017-07-10 21:09:47 +0800

    使用gulp压缩打包

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require("gulp");
+var rev = require("gulp-rev");
+var revReplace = require("gulp-rev-replace");
+var useref = require("gulp-useref");
+var filter = require("gulp-filter");
+var uglify = require("gulp-uglify");
+var csso = require("gulp-csso");
+
+gulp.task("default",function(){
+    var jsFilter = filter("**/*.js",{restore: true});
+    var cssFilter = filter("**/*.css",{restore: true});
+    var indexHtmlFilter = filter(["**/*","!**/index.html"],{restore: true});
+
+    var imgFilter = filter(["**/*.png","**/*.jpg","**/*.ico"],{restore: true});
+
+    gulp.src("src/index.html")
+        .pipe(useref())
+
+		.pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+
+        .pipe(cssFilter)
+        .pipe(csso())
+		.pipe(cssFilter.restore)
+
+        .pipe(indexHtmlFilter)
+		.pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+
+        .pipe(revReplace())
+
+        .pipe(gulp.dest("dist"));
+
+    gulp.src(["src/img/*.png","src/img/*.jpg","src/img/*.ico"])
+        .pipe(imgFilter)
+        .pipe(imgFilter.restore)
+
+        .pipe(gulp.dest("dist/img"));
+
+});
